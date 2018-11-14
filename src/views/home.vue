ba470f17f83eb5a6225aa736e0f9e54198c41cfb
baiyuming <baiyuming@zhizhangyi.com>
2018-11-14 15:24:59 +0800

    bym

--- src/views/home.vue
+++ src/views/home.vue
@@ -2,7 +2,7 @@
     <div class="single-page">
         <div class="banner">
            <div class="banner-bg">
-               <img src='../assets/logo.png' alt="">
+               <!--<img src='../assets/logo.png' alt="">-->
                 
            </div>
            <div class='btn'>
@@ -49,20 +49,20 @@ export default {
             display: inline-block;
             font-size: 24px;
             color: white;
-            &::before {
-                content: '';
-                display: inline-block;
-                width:400px;
-                left:-40px;
-                top:25px;
-                position: absolute;
-                z-index: 1;
-               right:0;bottom:0;
-                background:rgba(255,255,255,0);
-                border: 1px solid #fff;
-                border-top: 0 ;
-                transform: perspective(.5em) rotateX(-5deg);
-            }
+            /*&::before {*/
+                /*content: '';*/
+                /*display: inline-block;*/
+                /*width:400px;*/
+                /*left:-40px;*/
+                /*top:25px;*/
+                /*position: absolute;*/
+                /*z-index: 1;*/
+               /*right:0;bottom:0;*/
+                /*background:rgba(255,255,255,0);*/
+                /*border: 1px solid #fff;*/
+                /*border-top: 0 ;*/
+                /*transform: perspective(.5em) rotateX(-5deg);*/
+            /*}*/
 
         }
         img {
